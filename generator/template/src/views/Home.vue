aee777440ae4763a4ac6a72bc7012f28b6dc4550
DevTony101 <tonycortesano@gmail.com>
2020-09-08 00:04:29 -0500

    Adding index for generator. Rendering template

---
+++ generator/template/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home"></div>
+</template>
+
+<script>
+  export default {
+    name: "Home",
+  }
+</script>
+
+<style scoped>
+  .home {
+    display: block;
+  }
+</style>
