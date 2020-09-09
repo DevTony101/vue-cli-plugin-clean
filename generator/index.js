5d492244ffc52bba4c41c837ef44305362405782
DevTony101 <tonycortesano@gmail.com>
2020-09-09 02:01:42 -0500

    Succesfully deleting components directory. Creating base component

--- generator/index.js
+++ generator/index.js
@@ -1,8 +1,21 @@
+const fs = require("fs-extra");
+
 module.exports = (api) => {
+  const files = ["views/About.vue"];
+
   api.render("./template");
   api.extendPackage({
     dependencies: {
-      "lodash": "^4.17.19",
+      lodash: "^4.17.19",
     },
+  });
+
+  api.onCreateComplete(() => {
+    fs.emptyDirSync(api.resolve("src/components/"));
+    fs.mkdirSync(api.resolve("src/components/base"));
+    for (let i = 0; i < files.length; i++) {
+      const file = `src/${files[i]}`;
+      if (fs.existsSync(file)) fs.unlinkSync(api.resolve(file));
+    }
   });
 };
