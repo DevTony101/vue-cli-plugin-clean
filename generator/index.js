6b764d4c03d5395040b18de9f59c9bfaf9887e74
DevTony101 <tonycortesano@gmail.com>
2020-09-09 19:50:56 -0500

    Moving delete files logic outside hook

--- generator/index.js
+++ generator/index.js
@@ -2,18 +2,16 @@ const fs = require("fs-extra");
 
 module.exports = (api) => {
   const directories = ["views/", "components/"];
+  for (let i = 0; i < directories.length; i++) {
+    const dir = `src/${directories[i]}`;
+    fs.emptyDirSync(api.resolve(dir));
+  }
+  fs.mkdir(api.resolve("src/components/base"));
+  
   api.render("./template");
   api.extendPackage({
     dependencies: {
       lodash: "^4.17.19",
     },
-  });
-
-  api.onCreateComplete(() => {
-    for (let i = 0; i < directories.length; i++) {
-      const dir = `src/${directories[i]}`;
-      if (fs.ensureDirSync(dir)) fs.emptyDirSync(api.resolve(dir));
-    }
-    fs.mkdir(api.resolve("src/components/base"));
   });
 };
