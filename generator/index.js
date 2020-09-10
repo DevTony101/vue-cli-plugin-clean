0c4c904983408f5e4609d6badf01df56f6a0bbb9
DevTony101 <tonycortesano@gmail.com>
2020-09-09 22:14:55 -0500

    Succesfully creating configuration files for eslint and prettier

--- generator/index.js
+++ generator/index.js
@@ -1,6 +1,7 @@
 const fs = require("fs-extra");
 
 module.exports = (api) => {
+  const configFiles = [".eslintrc", ".prettierrc"];
   const directories = ["views/", "components/"];
   for (let i = 0; i < directories.length; i++) {
     const dir = `src/${directories[i]}`;
@@ -14,4 +15,21 @@ module.exports = (api) => {
       lodash: "^4.17.19",
     },
   });
+
+  api.onCreateComplete(() => {
+    const path = api.resolve("src/../");
+    for (let i = 0; i < configFiles.length; i++) {
+      const fromFile = `${__dirname}/../utils/${configFiles[i]}.txt`;
+      const toFile = `${path}/${configFiles[i]}.js`;
+      const data = fs.readFileSync(fromFile, options = {
+        encoding: 'utf8',
+        flag: 'r'
+      });
+
+      fs.writeFileSync(toFile, data, options = {
+        encoding: 'utf8',
+        flag: 'w'
+      });
+    }
+  })
 };
