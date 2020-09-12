ee658df0c31dcf86b106ed874ee6590c772d5eee
DevTony101 <tonycortesano@gmail.com>
2020-09-12 02:41:23 -0500

    Generating BaseIcon base component

---
+++ generator/template/src/components/base/BaseIcon.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg>
+    <use v-bind="{ 'xlink:href': source }" />
+  </svg>
+</template>
+
+<script>
+  export default {
+    name: "BaseIcon",
+    props: {
+      icon: {
+        type: String,
+        required: true,
+      },
+    },
+    computed: {
+      source: function() {
+        return `/icons.svg#${this.icon}`;
+      },
+    },
+  };
+</script>
