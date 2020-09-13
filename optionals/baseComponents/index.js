01e14be4e432ffc1a91d6b4058296fcfb3af83ce
DevTony101 <tonycortesano@gmail.com>
2020-09-13 00:17:01 -0500

    Fix issue with files

--- optionals/baseComponents/index.js
+++ optionals/baseComponents/index.js
@@ -1,8 +1,9 @@
 const fs = require("fs");
+const path = require("path");
   
 module.exports = function addBaseComponents(api) {
   const root = api.resolve("src/../");
-  fs.copyFileSync(`${__dirname}/resources/icons.svg`, `${root}/public/icons.svg`);
+  fs.copyFileSync(path.join(__dirname, "resources/icons.svg"), path.join(root, "public/icons.svg"));
 
   api.extendPackage({
     devDependencies: {
