8ffae11d2c87042cbff62b31083f5762ec15d813
DevTony101 <tonycortesano@gmail.com>
2020-09-12 19:18:49 -0500

    Successfully emptying the component and views folders. Leaving auto generated stuff.

--- generator/index.js
+++ generator/index.js
@@ -1,20 +1,31 @@
-const fs = require("fs-extra");
+const fs = require("fs");
+const path = require("path");
 const optionals = require("../optionals/index");
 
 module.exports = (api) => {
   const directories = ["views/", "components/"];
-  for (let i = 0; i < directories.length; i++) {
-    const dir = `src/${directories[i]}`;
-    fs.emptyDirSync(api.resolve(dir));
-  }
-  // fs.mkdir(api.resolve("src/components/base"));
 
   api.render("./template");
-  api.extendPackage({
-    dependencies: {
-      lodash: "^4.17.19",
-    },
+  optionals.prettierConfig(api);
+  optionals.scaffoldProject(api);
+
+  api.onCreateComplete(() => {
+    emptyDirs(directories);
   });
 
-  optionals.prettierConfig(api);
+  function emptyDirs(directories) {
+    for (let i = 0; i < directories.length; i++) {
+      const directory = `src/${directories[i]}`;
+      const dirPath = api.resolve(directory);
+      const files = fs.readdirSync(dirPath);
+      for (const file of files) {
+        const filePath = path.join(dirPath, file);
+        if (fs.lstatSync(filePath).isFile()) {
+          if (file !== "Home.vue") {
+            fs.unlinkSync(filePath);
+          }
+        }
+      }
+    }
+  }
 };
