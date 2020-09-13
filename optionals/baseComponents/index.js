8c06225231534c52b909ee93927234fc5e5acc66
DevTony101 <tonycortesano@gmail.com>
2020-09-12 23:32:32 -0500

    Using node's own solution for copy content

--- optionals/baseComponents/index.js
+++ optionals/baseComponents/index.js
@@ -1,4 +1,9 @@
+const fs = require("fs");
+  
 module.exports = function addBaseComponents(api) {
+  const root = api.resolve("src/../");
+  fs.copyFileSync(`${__dirname}/resources/icons.svg`, `${root}/public/icons.svg`);
+
   api.extendPackage({
     devDependencies: {
       lodash: "^4.17.19",
