d5d5beb9216c50ad9f866fee35184216b6696e37
DevTony101 <tonycortesano@gmail.com>
2020-09-14 18:23:30 -0500

    Adding description for VueUI integration

--- prompts.js
+++ prompts.js
@@ -5,12 +5,14 @@ module.exports = pkg => {
       name: 'scaffold',
       message: 'Add support for base components?',
       default: false,
+      description: 'In addition to deleting all pre-built components, it will add lodash and the necessary configuration for supporting base components.',
     },
     {
       type: 'confirm',
       name: 'tailwind',
       message: 'Add support for tailwind?',
       default: false,
+      description: 'This will install tailwind and add the necessary configuration files.',
     }
   ]
 
@@ -20,7 +22,8 @@ module.exports = pkg => {
       name: 'prettier',
       message: 'Add prettier configuration?',
       when: true,
-    })
+      description: 'This will add a prettier configuration file and add a vue/plugin-recommended plugin to your eslint configuration file.',
+    });
   }
 
   return prompts;
