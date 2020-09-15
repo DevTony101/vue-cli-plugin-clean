02bb3b6eebb389704199110744e854fab962562c
DevTony101 <tonycortesano@gmail.com>
2020-09-15 18:26:48 -0500

    Adding ui options

--- prompts.js
+++ prompts.js
@@ -5,14 +5,18 @@ module.exports = pkg => {
       name: "scaffold",
       message: "Add support for base components?",
       default: true,
+      group: "Strongly recommended",
       description: "In addition to deleting all pre-built components, it will add lodash and the necessary configuration for supporting base components.",
+      link: "https://github.com/DevTony101/vue-cli-plugin-clean#Features",
     },
     {
       type: "confirm",
       name: "tailwind",
       message: "Add support for tailwind?",
       default: true,
+      group: "Strongly recommended",
       description: "This will install tailwind and add the necessary configuration files.",
+      link: "https://github.com/DevTony101/vue-cli-plugin-clean#Features",
     }
   ]
 
@@ -22,7 +26,9 @@ module.exports = pkg => {
       name: "prettier",
       message: "Add prettier configuration?",
       default: true,
+      group: "Strongly recommended",
       description: "This will add a prettier configuration file and add a vue/plugin-recommended plugin to your eslint configuration file.",
+      link: "https://github.com/DevTony101/vue-cli-plugin-clean#Features",
     });
   }
 
