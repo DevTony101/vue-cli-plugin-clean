8a47e4111ec08b2a074e926a2a747e7efaf6f60e
Miguel Manjarres <54128874+DevTony101@users.noreply.github.com>
2020-12-11 00:37:53 -0500

    Delete: changings to eslint file

--- optionals/prettier/index.js
+++ optionals/prettier/index.js
@@ -3,32 +3,11 @@
 
 module.exports = function addPrettierConfig(api) {
   api.render((files) => {
-    files[".eslintrc.js"] = api.genJSConfig({
-      root: true,
-      env: {
-        node: true,
-      },
-      extends: [
-        "plugin:vue/essential",
-        "eslint:recommended",
-        "@vue/prettier",
-        "plugin:prettier/recommended",
-      ],
-      parserOptions: {
-        parser: "babel-eslint",
-      },
-      rules: {
-        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-      },
-    });
-
     files[".prettierrc.js"] = api.genJSConfig({
       trailingComma: "es5",
       vueIndentScriptAndStyle: true,
     });
   });
 
-  modifiedFiles.push(".eslintrc.js");
   createdFiles.push(".prettierrc.js");
 };
