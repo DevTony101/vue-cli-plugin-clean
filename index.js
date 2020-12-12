a0d2d44e637cf6e14e98c5b22707c9628a10fbac
Miguel Manjarres <54128874+DevTony101@users.noreply.github.com>
2020-12-12 17:48:14 -0500

    Registering basec command to service plugin instance

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-// Service Plugin
-module.exports = () => {};
+const basec = require("./commands/basec/index")
+
+module.exports = (api, options) => {
+  api.registerCommand("basec", {
+    description: "Generates a new base component",
+    usage: "vue-cli-service basec name [options]",
+    options: {
+      "name": "The name of the component",
+      "--scaffold-button": "Generates a predefined base component for a button",
+      "--preffix": "The preffix for the name of the component. By default 'Base'"
+    }
+  }, (args) => basec(args, api));
+};
