c21aa817856f129c60073a788b524e07d745c7ea
Miguel Manjarres <54128874+DevTony101@users.noreply.github.com>
2020-12-12 18:00:40 -0500

    Add: Logic for the basec command and template for the components

---
+++ commands/basec/index.js
@@ -0,0 +1,36 @@
+module.exports = (args, api) => {
+  const fs = require("fs");
+  const { EOL } = require('os');
+  const { RED } = require("./../../utils/colors");
+  const componentName = args._[0];
+  const scaffoldButton = args['scaffold-button'];
+  const preffix = args.preffix ?? "Base";
+
+  if (componentName || scaffoldButton) {
+    const filename = componentName ? `${preffix + capitalize(componentName)}` : "BaseButton";
+    let directory = `src/components/base/${filename}.vue`;
+    let dirPath = api.resolve(directory);
+    while (fs.existsSync(dirPath)) {
+      directory = `src/components/base/${filename}-${Math.random().toString(36).slice(-5)}.vue`;
+      dirPath = api.resolve(directory);
+    }
+    const strMatch = scaffoldButton ? /name: "BaseButton",/ : /name: "base",/;
+    const content = replaceContent(`${__dirname}/templates/Base${scaffoldButton ? "Button" : ""}.vue`, strMatch, `  name: "${filename}",`);
+    fs.writeFileSync(dirPath, content.join(EOL), { encoding: 'utf-8' });
+    console.log(`\n${filename} created succesfully`);
+  } else {
+    console.log(RED, "\nError: You must supply either a name or a scaffold option");
+  }
+
+  function replaceContent(path, strMatch, strReplace) {
+    const content = fs.readFileSync(path, { encoding: 'utf-8' });
+    const lines = content.split(/\r?\n/g);
+    const renderIndex = lines.findIndex(line => line.match(strMatch));
+    lines[renderIndex] = strReplace;
+    return lines;
+  }
+
+  function capitalize(str) {
+    return str[0].toUpperCase() + str.substring(1);
+  }
+}
---
+++ commands/basec/templates/BaseButton.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <button v-on="$listeners" class="button" v-bind="$attrs">
+      <slot />
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseButton",
+  inheritAttrs: false,
+};
+</script>
+
+<style scoped>
+.button {
+  height: 25px;
+  width: 75px;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  white-space: nowrap;
+  transition: all 0.2s linear;
+}
+
+.button:hover {
+  -webkit-transform: scale(1.01);
+  transform: scale(1.01);
+  box-shadow: 0 2px 12px -5px rgba(0, 0, 0, 0.75);
+}
+
+.button:active {
+  -webkit-transform: scale(1);
+  transform: scale(1);
+  box-shadow: none;
+}
+
+.button:focus {
+  outline: 0;
+}
+</style>
